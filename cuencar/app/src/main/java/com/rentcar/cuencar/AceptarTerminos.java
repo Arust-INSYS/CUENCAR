7ed700a5743eaa2a0655fa01da3e0b855e4fd362
IgnacioJuan <juan.guachichullca.est@tecazuay.edu.ec>
2022-06-28 17:53:17 -0500

    Creacion Activities

--- cuencar/app/src/main/java/com/rentcar/cuencar/AceptarTerminos.java
+++ cuencar/app/src/main/java/com/rentcar/cuencar/AceptarTerminos.java
@@ -10,5 +10,6 @@ public class AceptarTerminos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aceptar_terminos);
+        //123
     }
 }
