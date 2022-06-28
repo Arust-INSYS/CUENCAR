b431b624ca2f7e6807e793e2805952efbd3ad26d
IgnacioJuan <juan.guachichullca.est@tecazuay.edu.ec>
2022-06-28 17:42:12 -0500

    Creacion Activities

---
+++ cuencar/app/src/main/java/com/rentcar/cuencar/AceptarTerminos.java
@@ -0,0 +1,14 @@
+package com.rentcar.cuencar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AceptarTerminos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aceptar_terminos);
+    }
+}
