bdfe6e62901a8079183e73babe4014126f5d64a3
DavidSisalima7 <97250207+DavidSisalima7@users.noreply.github.com>
2022-06-28 18:17:07 -0500

    Creacion de interfaces Menu principal. Informacion. Alquiler de vehiculo.

--- cuencar/app/src/main/java/com/rentcar/cuencar/Alquiler_Vehiculo.java
+++ cuencar/app/src/main/java/com/rentcar/cuencar/Alquiler_Vehiculo.java
@@ -1,14 +1,14 @@
 package com.rentcar.cuencar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class pantalla_editar extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Alquiler_Vehiculo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pantalla_editar);
+        setContentView(R.layout.activity_alquiler_vehiculo);
     }
 }
