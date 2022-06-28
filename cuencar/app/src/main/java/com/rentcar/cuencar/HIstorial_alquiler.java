2f1c9346164770e6664f7e40caafb8ca2c8fe6ed
Johnquiroga12 <100298882+Johnquiroga12@users.noreply.github.com>
2022-06-28 16:12:31 -0500

    john
    axtiviti

---
+++ cuencar/app/src/main/java/com/rentcar/cuencar/HIstorial_alquiler.java
@@ -0,0 +1,14 @@
+package com.rentcar.cuencar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HIstorial_alquiler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial_alquiler);
+    }
+}
