6df4d9cde426087a6e4b28e35a57a6f06b7e33b8
Johnquiroga12 <100298882+Johnquiroga12@users.noreply.github.com>
2022-06-29 17:40:37 -0500

    john
    ya esta

--- cuencar/app/src/main/java/com/rentcar/cuencar/perifill_act.java
+++ cuencar/app/src/main/java/com/rentcar/cuencar/perifill_act.java
@@ -2,17 +2,18 @@ package com.rentcar.cuencar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class perifill_act extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-<<<<<<< Updated upstream
+        setContentView(R.layout.activity_perifill_act);
         asignacion();
-        asignacion_pantalla_historial();
     }
 
     private void asignacion(){
@@ -22,20 +23,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent inten=new Intent(getApplicationContext(),pantalla_editar.class);
-                startActivity(inten);
-            }
-        });
-=======
-        //Prueba123456789fghjk
->>>>>>> Stashed changes
-    }
-    private void asignacion_pantalla_historial(){
-        Button his;
-        his=findViewById(R.id.btnmihistoria);
-        his.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent inten=new Intent(getApplicationContext(),HIstorial_alquiler.class);
                 startActivity(inten);
             }
         });
