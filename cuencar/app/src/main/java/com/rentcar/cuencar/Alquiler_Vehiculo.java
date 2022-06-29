0d0cbd1a28bb2b87b5b1a9756eb20d26c2a4441f
DavidSisalima7 <97250207+DavidSisalima7@users.noreply.github.com>
2022-06-29 18:36:57 -0500

    Conector Map

--- cuencar/app/src/main/java/com/rentcar/cuencar/Alquiler_Vehiculo.java
+++ cuencar/app/src/main/java/com/rentcar/cuencar/Alquiler_Vehiculo.java
@@ -1,6 +1,9 @@
 package com.rentcar.cuencar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,18 @@ public class Alquiler_Vehiculo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alquiler_vehiculo);
+    }
+
+    private void IniciaControl(){
+        Button btnBusqueda;
+        btnBusqueda=findViewById(R.id.btnBuscar);
+        btnBusqueda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inten = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(inten);
+            }
+        });
+
     }
 }
