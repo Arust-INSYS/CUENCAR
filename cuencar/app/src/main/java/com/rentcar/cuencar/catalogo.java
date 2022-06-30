a98c691d2dc253f10d4abe7daecf55e565f1dd8a
Arust-INSYS <90620456+Arust-INSYS@users.noreply.github.com>
2022-06-29 20:50:54 -0500

    Cambios MenuPrincipal

--- cuencar/app/src/main/java/com/rentcar/cuencar/catalogo.java
+++ cuencar/app/src/main/java/com/rentcar/cuencar/catalogo.java
@@ -9,6 +9,6 @@ public class catalogo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_catalogo);
+        setContentView(R.layout.activity_catalog);
     }
 }
