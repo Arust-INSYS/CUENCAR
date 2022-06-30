7c57be5e87100eecf58bb9aea74c9c98454668d3
Arust-INSYS <90620456+Arust-INSYS@users.noreply.github.com>
2022-06-29 21:00:54 -0500

    comentario_logo

--- cuencar/app/src/main/java/com/rentcar/cuencar/catalogo.java
+++ cuencar/app/src/main/java/com/rentcar/cuencar/catalogo.java
@@ -10,5 +10,6 @@ public class catalogo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+        //comentario
     }
 }
